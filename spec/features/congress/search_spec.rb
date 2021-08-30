c9d83c510b9b656800d9a72e341da7e26151412b
Robert Gu <robertgu84@gmail.com>
2021-08-30 08:18:37 -0600

    Fix specs

--- spec/features/congress/search_spec.rb
+++ spec/features/congress/search_spec.rb
@@ -10,9 +10,15 @@ RSpec.describe 'Search for members of congress' do
   it 'Can search for members of congress' do
 
     response_body = File.read('spec/fixtures/senators.json')
-
     stub_request(:get, "https://api.propublica.org/congress/v1/117/senate/members.json").
-         to_return(status: 200, body: response_body, headers: {})
+            with(
+              headers: {
+             'Accept'=>'*/*',
+             'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+             'User-Agent'=>'Faraday v1.5.0',
+             'X-Api-Key'=> "xxhtODvy6FBCaBl8BM5d8SggVU417aRruASdTrRg"
+              }).
+              to_return(status: 200, body: response_body, headers: {})
 
     visit root_path
 
